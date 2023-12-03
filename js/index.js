f2c34273fbfa08a28a06ded2c18b458d2df1561b
SineYlo <vavshhik01@mail.ru>
2023-12-03 11:14:42 +1000

    feat: added markup and styles for the header

--- js/index.js
+++ js/index.js
@@ -20,3 +20,23 @@ function buttonAction(){
 };
 
 buttonAction() ;
+
+const hamburgerButton = document.querySelector('.hamburger');
+const hamburgerMenu = document.querySelector('.navigation-wrapper');
+const headerBorder = document.querySelector('.site-header__content');
+
+hamburgerButton.addEventListener('click', function () {
+  hamburgerButton.classList.toggle('is-active');
+  hamburgerMenu.classList.toggle('is-active');
+  headerBorder.classList.toggle('is-active');
+
+  if (
+    [hamburgerButton, hamburgerMenu, headerBorder].every((element) =>
+      element.classList.contains('is-active')
+    )
+  ) {
+    scrollLock.disablePageScroll();
+  } else {
+    scrollLock.enablePageScroll();
+  }
+});
